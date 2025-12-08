d55652351203df8214ea898c2e01e8917125d5ca
Justin <94926541+secadaIO@users.noreply.github.com>
2025-12-08 07:20:37 -0500

    Update shields.sol

--- v1.5/shields.sol
+++ v1.5/shields.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-//
+// <p>Link to Genesis Anchor commit CIP v5.1:</p>
+
+[![Cardano Anchor](https://img.shields.io/badge/Cardano-Anchored%20🕊️-blueviolet)](https://explorer.cardano.org/en/transaction?id=0a5d16d46ab3d696d95aa4423f2362a37daac24bab54f905c3d704cdb19f31c9)
 pragma solidity ^0.8.20;
 
 /*
